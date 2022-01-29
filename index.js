7c4f3b7e6e26c67aba5920a9a7103e0c41ce1305
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-29 15:22:54 -0500

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,34 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(element) {
+    element = element.split(" ");
+    for (let i = 0, n = element.length; i < n; i++) {
+      element[i]= element[i][0].toUpperCase() + element[i].substring(1)
+    }
+    element = element.join(" ");
+    return element;
+    //console.log(element);
+  })
 }
+
+
+  
+
+
+/* Go Back And Ask For Help On This Function 
+
+const judge = tutorials.map(function (element) { 
+  element = element[0].toUpperCase() + element.substring(1);
+  console.log(element);
+  for(let i = 0, n = element.length; i < n ; i++) {
+    if(element[i] === ' ') {
+     element[i + 1] = element[i + 1].toUpperCase();
+      console.log(element[i + 1]);
+    }
+  }  
+})
+
+judge;
+
+*/
